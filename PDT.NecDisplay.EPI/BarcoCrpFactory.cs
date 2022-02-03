94b91346caf1baee7380fcb4945d9e1b88f2482f
Jason Alborough <jalborough@pepperdash.com>
2022-02-03 15:04:53 -0500

    wip: initial seed of project from nec display epi

---
+++ PDT.NecDisplay.EPI/BarcoCrpFactory.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Crestron.SimplSharp;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using PepperDash.Essentials.Core.Config;
+using System.Collections;
+using PepperDash.Essentials.Bridges;
+using PepperDash.Essentials.Core;
+
+namespace PDT.BarcoCrp.EPI
+{
+	public class PdtBarcoCrpEpi
+	{
+		public static void LoadPlugin()
+		{
+			PepperDash.Essentials.Core.DeviceFactory.AddFactoryForType("BarcoCrp", PdtBarcoCrpEpi.BuildDevice);
+		}
+
+		public static string MinimumEssentialsFrameworkVersion = "1.10.0";
+
+		public static PdtBarcoCrp BuildDevice(DeviceConfig dc)
+		{
+			var config = JsonConvert.DeserializeObject<DeviceConfig>(dc.Properties.ToString());
+			var comm = CommFactory.CreateCommForDevice(dc);
+            try
+            {
+               // if there is no id in the config file an exception is thrown
+               var newMe = new PdtBarcoCrp(dc.Key, dc.Name, comm, dc.Properties["id"].Value<string>());
+               return newMe;
+            }
+            catch
+            {
+                // if there is no id in the config file an exception is thrown.  id will default to (0x2a) the all displays command
+                var newMe = new PdtBarcoCrp(dc.Key, dc.Name, comm);
+                return newMe;
+            }
+		}
+	}
+}
